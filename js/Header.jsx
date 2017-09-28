73b4e6af4b0f92e2af5aada2b0f55e0e3bdca7df
moxdev <moxdev1@gmail.com>
2017-09-28 17:27:00 -0400

    video-51 - video-54

--- js/Header.jsx
+++ js/Header.jsx
@@ -3,21 +3,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Header = (props: { showSearch?: boolean }) => {
+const Header = (props: { showSearch?: boolean, handleSearchTermChange?: Function, searchTerm?: string }) => {
   let utilSpace;
   if (props.showSearch) {
-    utilSpace = <h1>lol</h1>;
+    utilSpace = (
+      <input onChange={props.handleSearchTermChange} value={props.searchTerm} type="text" placeholder="Search" />
+    );
   } else {
     utilSpace = (
       <h2>
-        <Link to="/search">Back</Link>
+        <Link to="/search">
+          Back
+        </Link>
       </h2>
     );
   }
   return (
     <header>
       <h1>
-        <Link to="/">svideo</Link>
+        <Link to="/">
+          svideo
+        </Link>
       </h1>
       {utilSpace}
     </header>
@@ -25,6 +31,9 @@ const Header = (props: { showSearch?: boolean }) => {
 };
 
 Header.defaultProps = {
-  showSearch: false
+  showSearch: false,
+  handleSearchTermChange: function noop() {},
+  searchTerm: ''
 };
+
 export default Header;
